ea3b6910f33e46cdc6ffa53a40d53c4179650b9a
Paul Vaughan <paulieboo@gmail.com>
2021-08-13 20:59:54 +0100

    New linter 'Revive' and config; go.mod/sum updates.

--- format.sh
+++ format.sh
@@ -7,12 +7,19 @@ echo -e "\e[1mRunning 'go vet ./...'\e[0m"
 go vet ./...
 
 echo -e "\e[1mRunning 'golint ./...'\e[0m"
-golint ./...
+golint --min_confidence 0.2 ./...
 
 echo -e "\e[1mRunning 'staticheck ./...'\e[0m"
 staticcheck ./...
 
 echo -e "\e[1mRunning 'go tool fix --diff .'\e[0m"
 go tool fix --diff .
+
+echo -e "\e[1mRunning 'revive ./...'\e[0m"
+revive -config revive.toml ./...
+
+echo -e "\e[1mRunning 'golangci-lint run'\e[0m"
+# golangci-lint run --enable-all
+golangci-lint run
 
 echo -e "\e[1mDone.\e[0m"
--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.16
 require (
 	code.cloudfoundry.org/bytefmt v0.0.0-20210608160410-67692ebc98de
 	github.com/hako/durafmt v0.0.0-20210608085754-5c1018a4e16b
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
 )
