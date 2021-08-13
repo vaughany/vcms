5e77bf2aaffa75e6fb6cd1a5efd36393c8e0dbf9
Paul Vaughan <paulieboo@gmail.com>
2021-08-13 21:20:16 +0100

    Using GoReleaser (Gophers Slack suggestion) instead of committing binaries, plus convenience script.

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Test it.
+# goreleaser release --snapshot --skip-publish --rm-dist
+
+# Release it.
+#   --skip-validate ignores git's current state.
+#   --rm-dist clears out the ./dist folder first.
+goreleaser release --skip-validate --rm-dist
